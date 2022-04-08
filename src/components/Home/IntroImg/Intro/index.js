b991c0a48f8f6a5ae8e5d54ae0474aaaeb22b7f4
wakenedo <a_nuner@outlook.com>
2022-04-08 11:10:36 -0300

    Update index.js

--- src/components/Home/IntroImg/Intro/index.js
+++ src/components/Home/IntroImg/Intro/index.js
@@ -6,22 +6,19 @@ export default function Intro() {
             <section className={styles.section}>
                 <section className={styles.container}>
                     <p>
-                        Programador autodidata, pai dedicado e apaixonado por tecnologia.
-                        Sempre aberto para aprender e me aprimorar.
-                        Comecei a programar em 2017, principalmente com JS vanilla, CSS puro e HTML,
-                        fazendo pequenas mudanças, páginas estáticas, landing pages.
-                        Depois de um tempo trabalhando com JavaScript, conheci o ReactJS através da comunidade
-                        e como os projetos maiores usavam algum tipo de biblioteca em geral, comecei a aprender o
-                        ReactJS devido às análises e recursos da ferramenta. Após alguns meses de estudo e prática.
-                        Entrei em pequenos projetos ReactJS, participando de ambientes de desenvolvimento mais dinâmicos.
-                        Nesses projetos, aprendi a trabalhar como membro da equipe lado a lado com outros desenvolvedores,
-                        marketing, QAs, PMOs. Lá eu tive contato com agile como Scrum e Kanban e Git para versionamento.
-                        <br />Já trabalhei em mais alguns projetos, com clientes locais e estrangeiros como desenvolvedor
-                        front end com foco em web. Onde consolidei o que aprendi com meus primeiros projetos,
-                        e comecei a aprender TypeScript, Redux, Axios e junto com outras das principais tecnologias
-                        do ecossistema ReactJS. De 2017 até agora, tenho aprendido cada vez mais sobre minha pilha
-                        escolhida anteriormente, com o objetivo de aprender mais sobre a linguagem e tecnologia,
-                        bem como aprender mais sobre o processo de back-end e processo de desenvolvimento completo.
+                    Self-taught programmer, that means, really, always learning from others, dedicated and passionate about technology.
+Always open to learn and improve. On a journey of self-development and improving as a software-developer.
+I'm a very organized, communicative and curious individual. I like being part of team and achieve objectives
+together, organizing strategies as each team member contributes with his abilities and experieces to deal 
+with problems, issues and solutions can be a lot of fun and very rewarding as a growth experience. 
+                        <br /> Being part of a team is extremely important to develop skills, teams are able to provide insights that you 
+could never access alone. I actually found out I love to learn and researching, and since I was born in the 
+early 90s and happened to see the technology boom we had in our lives, and how that affected every aspect
+of our lives I was naturaly draw to it. Currently Im lookign to switch from freelancing to full position as a 
+software developer, I'm not afraid of starting from the bottom, and working my way up. As I've stated before, 
+I love learning, and I realise that the skills of being a freelancer might not be as usefull or alingned with 
+what I'm looking for. So im open to join a team and learn as much as I can, and bringing to table my experience, skills and undivided
+attention to the project. If you wanna have a talk, and thinks my profile fits you're position lets have chat.  
                     </p>
                 </section>
             </section>
