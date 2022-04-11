364b84e0d6482b975c12617d54b316e6657441a2
wakenedo <a_nuner@outlook.com>
2022-04-11 15:24:22 -0300

    Update index.js

--- src/components/Home/Resume/Experience/index.js
+++ src/components/Home/Resume/Experience/index.js
@@ -10,24 +10,26 @@ export default function Experience() {
                 <h3><GiDiploma />Experience</h3>
                 <div>
                     <br />
-                    <h4>Frontend Web Developer</h4>
+                    <h4>Frontend Web Dev.</h4>
                     <>
                         <span>
-                            Desenvolvimento web frontend, utilizando JavaScript / ReactJS / TypeScript criando interfaces
-                            intuitivas, a partir de protótipos elaborados pelos designers.
-                            Projetos responsivos, código estático e dinâmico. Código limpo, CSS hierárquico, busca e
-                            roteamento de API. Consertando rotas, adicionando novos ou consertando componentes antigos,
-                            testes automatizados, análise de desempenho.
+                            Frontend web development, working with JScript and ReactJS with typing using TypeScript.  
+                            Developing intuitive and responsive interfaces, using modern ReactJS syntax and patterns.
+                            Developed projects from prototypes made by the designers(Figma & AdobeXD), and sometimes myself when needed mainly using Figma.
+                            Hierarchical CSS,SASS and Styled-Components CMS(Prismic.io), declarative interfaces, depending on the project rules of business and goals,
+                            or my role in the project. State-management using Redux and or Context API, for more complex applications.
+                            Working sometimes in an agile environment where tasks are solicited as a chain of tasks or a single in a timeline accordingly to each developer
+                            role and responsibilities in the project. 
                         </span>
                     </>
                 </div>
                 <div>
-                    <h4>Ferramentas & Tecnologias</h4>
+                    <h4>Tools & Techs</h4>
                     <section >
-                        <span>- Figma (Prototipação) </span>
+                        <span>- Figma (Prototyping) </span>
                         <span>- Agile</span>
-                        <span>- Kanban e Scrum</span>
-                        <span>- GitHub(Versionamento)</span>
+                        <span>- Kanban and Scrum</span>
+                        <span>- GitHub(Versioning)</span>
                     </section>
                     <h4>Frontend</h4>
                     <section >
