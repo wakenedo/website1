b4fdfd44ba69af8e7ac90da50ab90fb6f7476950
wakenedo <a_nuner@outlook.com>
2022-05-26 17:28:09 -0300

    Grammar fixes

--- src/components/Home/IntroImg/Intro/index.js
+++ src/components/Home/IntroImg/Intro/index.js
@@ -9,14 +9,14 @@ export default function Intro() {
                     Self-taught programmer, that means, really, always learning from others, dedicated and passionate about technology.
 Always open to learn and improve. On a journey of self-development and improving as a software-developer.
 I'm a very organized, communicative and curious individual. I like being part of team and achieve objectives
-together, organizing strategies as each team member contributes with his abilities and experieces to deal 
+together, organizing strategies as each team member contributes with his abilities and experience to deal 
 with problems, issues and solutions can be a lot of fun and very rewarding as a growth experience. 
                         <br /> Being part of a team is extremely important to develop skills, teams are able to provide insights that you 
 could never access alone. I actually found out I love to learn and researching, and since I was born in the 
 early 90s and happened to see the technology boom we had in our lives, and how that affected every aspect
-of our lives I was naturaly draw to it. Currently Im lookign to switch from freelancing to full position as a 
+of our lives I was naturally draw to it. Currently Im looking to switch from freelancing to full position as a 
 software developer, I'm not afraid of starting from the bottom, and working my way up. As I've stated before, 
-I love learning, and I realise that the skills of being a freelancer might not be as usefull or alingned with 
+I love learning, and I realise that the skills of being a freelancer might not be as useful or aligned with 
 what I'm looking for. So im open to join a team and learn as much as I can, and bringing to table my experience, skills and undivided
 attention to the project. If you wanna have a talk, and thinks my profile fits you're position lets have a chat.  
                     </p>
