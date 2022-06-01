e6f1c7ac05d43d60f293cf4c888ef4767caa1cf2
wakenedo <a_nuner@outlook.com>
2022-06-01 13:55:38 -0300

    Updated Experience Section
    Have to fix the responsiveness of the updated section.

--- src/components/Home/Experience/index.js
+++ src/components/Home/Experience/index.js
@@ -7,54 +7,96 @@ export default function Experience() {
     return (
         <main className={styles.content}>
             <section className={styles.section}>
+                <div>
                 <h3><GiDiploma />Experience</h3>
-                <div>
-                    <br />
-                    <h4>Frontend Web Dev.</h4>
-                    <>
-                        <span>
-                            Frontend web development, working with JScript and ReactJS with typing using TypeScript.  
-                            Developing intuitive and responsive interfaces, using modern ReactJS syntax and patterns.
-                            Developed projects from prototypes made by the designers(Figma & AdobeXD), and sometimes myself when needed mainly using Figma.
-                            Hierarchical CSS,SASS and Styled-Components CMS(Prismic.io), declarative interfaces, depending on the project rules of business and goals,
-                            or my role in the project. State-management using Redux and or Context API, for more complex applications.
-                            Working sometimes in an agile environment where tasks are solicited as a chain of tasks or a single in a timeline accordingly to each developer
-                            role and responsibilities in the project. 
-                        </span>
-                    </>
                 </div>
-                <div>
-                    <h4>Tools & Techs</h4>
-                    <section >
-                        <span> Figma (Prototyping) </span>
-                        <span> Agile</span>
-                        <span> Kanban and Scrum</span>
-                        <span> GitHub(Versioning)</span>
-                    </section>
-                    <h4>Frontend</h4>
-                    <section >
-                        <span> HTML </span>
-                        <span> React.js</span>
-                        <span> Redux</span>
-                        <span> Socket.io</span>
-                        <span> Axios</span>
-                        <span> TypeScript</span>
-                        <span> CSS</span>
-                        <span> Styled-Components</span>
-                        <span> SASS</span>
-                    </section>
-                    <h4>Backend</h4>
-                    <section >
-                        <span> Node.js </span>
-                        <span> Express </span>
-                        <span> MongoDb </span>
-                        <span> Firestore </span>
-                    </section>
-                    <h4>Freelancer | May 2019 until Date</h4>
-                </div>
-
+                <section className={styles.resumeItems}>
+                    <section className={styles.resumeItem}>
+                        <div>
+                            <h4>Freelancer | May 2019 until Date</h4>
+                            <br />
+                            <h4>Frontend Web Dev.</h4>
+                            <section className={styles.resumeItemPosition}>
+                                <span>
+                                    Frontend web development, working with JavaScript and ReactJS with typing using TypeScript.
+                                    Developing intuitive and responsive interfaces, using modern ReactJS syntax and patterns.
+                                    Developed projects from prototypes made by the designers(Figma & AdobeXD), and sometimes myself when needed mainly using Figma.
+                                    Hierarchical CSS,SASS and Styled-Components CMS(Prismic.io), declarative interfaces, depending on the project rules of business and goals,
+                                    or my role in the project. State-management using Redux and or Context API, for more complex applications.
+                                    Working sometimes in an agile environment where tasks are solicited as a chain of tasks or a single in a timeline accordingly to each developer
+                                    role and responsibilities in the project.
+                                </span>
+                            </section>
+                        </div>
+                        <h4>Tools & Techs</h4>
+                        <section className={styles.resumeItemTools}>
+                            <p> Figma (Prototyping), </p>
+                            <p> Agile,</p>
+                            <p> Kanban and Scrum,</p>
+                            <p> GitHub(Versioning)</p>
+                        </section>
+                        <h4>Frontend</h4>
+                        <section className={styles.resumeItemFront}>
+                            <p> HTML, </p>
+                            <p> React.js,</p>
+                            <p> Redux,</p>
+                            <p> Socket.io,</p>
+                            <p> Axios,</p>
+                            <p> TypeScript,</p>
+                            <p> CSS,</p>
+                            <p> Styled-Components,</p>
+                            <p> SASS </p>
+                        </section>
+                        <h4>Backend</h4>
+                        <section className={styles.resumeItemBack}>
+                            <p> Node.js, </p>
+                            <p> Express, </p>
+                            <p> MongoDb, </p>
+                            <p> Firestore </p>
+                        </section>
+                        <div />
+                    </section >
+                    <section className={styles.resumeItem}>
+                        <div>
+                            <h4>BunniesHole | Jan 2022 until Date</h4>
+                            <br />
+                            <h4>Frontend Web Dev.</h4>
+                            <section className={styles.resumeItemPosition}>
+                                <span>
+                                    Frontend web development, working with JavaScript and ReactJS using TypeScript.
+                                    Developing intuitive and responsive interfaces, using modern ReactJS syntax and patterns.
+                                    Developed interfaces, e-mail and components from prototypes made by the designers(Figma).
+                                    or my role in the project. State-management using Redux and or Context API, for more complex applications.
+                                    Working sometimes in an agile environment where tasks are solicited as a chain of tasks or a single in a timeline accordingly to each developer
+                                    role and responsibilities in the project.
+                                </span>
+                            </section>
+                        </div>
+                        <h4>Tools & Techs</h4>
+                        <section className={styles.resumeItemTools}>
+                            <p> Figma (Prototyping), </p>
+                            <p> Agile,</p>
+                            <p> GitHub(Versioning)</p>
+                        </section>
+                        <h4>Frontend</h4>
+                        <section className={styles.resumeItemFront}>
+                            <p> HTML, </p>
+                            <p> React.js,</p>
+                            <p> Redux,</p>
+                            <p> Socket.io,</p>
+                            <p> Axios,</p>
+                            <p> TypeScript,</p>
+                            <p> Styled-Components</p>
+                        </section>
+                        <h4>Backend</h4>
+                        <section className={styles.resumeItemBack}>
+                            <p> Node.js, </p>
+                            <p> Express </p>
+                        </section>
+                        <div />
+                    </section> 
+                </section>
             </section>
-
-        </main>
+        </main >
     )
 }
