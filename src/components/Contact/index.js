a2d33bea37b8882cbd4ceae16aa989a5e19eb06c
wakenedo <a_nuner@outlook.com>
2022-07-18 15:12:17 -0300

    Contact Functionality implemented
    Now the contact component is working as intended, you can minimize and maximize it, allowing users to click on the contact button , to minimize and maximize it. Working for both version Desktop and Mobile

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,13 +5,71 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 
 import styles from './contact.module.scss'
-import { HomeOutlined, KeyboardArrowDownOutlined } from '@material-ui/icons';
+import { HomeOutlined, KeyboardArrowDownOutlined, KeyboardArrowUpOutlined } from '@material-ui/icons';
+
 //AccountTreeOutlined
 function Contact({ isOpen, toggle }) {
-    console.log('contact','toggle log', toggle , isOpen)
-    return (
-        <>
+    console.log('contact', 'toggle log', toggle, isOpen)
+
+    if (isOpen === true) {
+
+        return (
+
+            <>
+
+                <main className={styles.contact}>
+                    <div className={styles.contactMenu}>
+                        <a href='https://alexandre-alvarenga-developer.netlify.app/'>
+                            <HomeOutlined />
+                            <p>
+                                Home
+                            </p>
+                        </a>
+                        {/*<a href='https://alexandre-alvarenga-developer.netlify.app/Projects'>
+                            <AccountTreeOutlined />
+                            <p>
+                                Projects
+                            </p>
+                        </a>*/}
+                        <div className={styles.contactMenuNav} >
+                            <button onClick={toggle}>
+                                <span>
+                                    Contact
+                                </span>
+                                <KeyboardArrowDownOutlined />
+                            </button>
+                        </div>
+
+                    </div>
+                    <section>
+
+
+                        <p><a href="https://www.linkedin.com/in/alexandre-alvarenga-86841b1b8/"><LinkedInIcon /> <span>LinkedIn : @alexandre-alvarenga</span></a></p>
+
+
+
+
+                        <p><a href="https://outlook.com/"><MailOutlineIcon /> <span>E-mail : a_nuner@outlook.com</span></a></p>
+
+
+
+
+                        <p><a href="https://github.com/wakenedo"><GitHubIcon /> <span>GitHub : @wakenedo</span></a></p>
+
+
+
+
+                        <p><a href="https://www.skype.com/pt-br/"><PhoneAndroidIcon /> <span>+55 (11) 9 6184-8402</span></a></p>
+
+                    </section>
+
+                </main>
+            </>
+        )
+    } if (isOpen === false) {
+        return (
             <main className={styles.contact}>
+
                 <div className={styles.contactMenu}>
                     <a href='https://alexandre-alvarenga-developer.netlify.app/'>
                         <HomeOutlined />
@@ -20,46 +78,25 @@ function Contact({ isOpen, toggle }) {
                         </p>
                     </a>
                     {/*<a href='https://alexandre-alvarenga-developer.netlify.app/Projects'>
-                        <AccountTreeOutlined />
-                        <p>
-                            Projects
-                        </p>
-                    </a>*/}
+                            <AccountTreeOutlined />
+                            <p>
+                                Projects
+                            </p>
+                        </a>*/}
                     <div className={styles.contactMenuNav} >
                         <button onClick={toggle}>
                             <span>
                                 Contact
                             </span>
-                            <KeyboardArrowDownOutlined />
+
+                            <KeyboardArrowUpOutlined />
                         </button>
                     </div>
-                            
+
                 </div>
-                <section>
-
-
-                    <p><a href="https://www.linkedin.com/in/alexandre-alvarenga-86841b1b8/"><LinkedInIcon /> <span>LinkedIn : @alexandre-alvarenga</span></a></p>
-
-
-
-
-                    <p><a href="https://outlook.com/"><MailOutlineIcon /> <span>E-mail : a_nuner@outlook.com</span></a></p>
-
-
-
-
-                    <p><a href="https://github.com/wakenedo"><GitHubIcon /> <span>GitHub : @wakenedo</span></a></p>
-
-
-
-
-                    <p><a href="https://www.skype.com/pt-br/"><PhoneAndroidIcon /> <span>+55 (11) 9 6184-8402</span></a></p>
-
-                </section>
-
             </main>
-        </>
-    )
+        )
+    }
 }
 
 export default Contact
